2233a96fbac13fe0bfa0f0de8206973ba56ec22c
Salik Hussain <salikhussain71@gmail.com>
2026-05-06 10:43:02 +0500

    week 0 day 1 : print function and first debug
    learned function need (). fixed syntex error and ran succefully

---
+++ week-01/day01.py
@@ -0,0 +1,8 @@
+CS50P Week 0 - Day 1
+Topic: print() function and SyntaxError debug
+Goal: PAKGOV-RAG foundation starts here
+
+print("Hello, World")
+print("My name is Salik")
+print("I am learning Python for the Pak Government RAG system")
+print("This will fail")

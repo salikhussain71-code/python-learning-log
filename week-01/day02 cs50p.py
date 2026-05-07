feffd613a327e61db2ece7b903e237f7e59bdc4a
Salik Hussain <salikhussain71@gmail.com>
2026-05-07 16:59:17 +0500

    Day 2 : CS50P strings ,strip ,title split , f-string

---
+++ week-01/day02 cs50p.py
@@ -0,0 +1,8 @@
+# day 2 - CS50P LECTURE 0 
+#may 7 , 2026 - Salik Hussain
+
+name = input("whats your name? ") .strip().title()
+first,last = name.split(" ")
+
+print(f"hello, {first}")
+print("today i learned: input ,print ,strip, title,f-strings")

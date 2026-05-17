e404ba3054bf5597d4fab2cb48b6344f718c6af2
Salik Hussain <salikhussain71@gmail.com>
2026-05-17 14:59:48 +0500

    Day 08: upper(), lower(), swapcase() with input()
    Week 2 Day 02. Practiced all three string case methods. Fixed print quotes bug in swapcase.

---
+++ week02/day02.py
@@ -0,0 +1,14 @@
+# lower
+salik = input("Enter name: ")
+salik_lower = salik.lower()
+print(salik_lower)
+
+# upper
+hussain = input("Enter name: ")
+hussain_upper = hussain.upper()
+print(hussain_upper)
+
+# swapcase
+salik = input("Enter name: ")
+salik_swap = salik.swapcase()
+print(salik_swap)

8a3de1857b8b5c146d7ef71fce11d6f72a5df17a
Salik Hussain <salikhussain71@gmail.com>
2026-05-18 14:44:45 +0500

    Create day-03_if.py
    Used separate if statements with <, >, ==. Tested with 1 2, 2 1, 1 1.`

---
+++ week02/week-02/day-03_if.py
@@ -0,0 +1,9 @@
+x = int(input("what x? "))
+y = int(input("what's y? "))
+
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal to y")

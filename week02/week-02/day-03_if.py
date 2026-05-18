1aef743b4b1ec39b246fa02d04d6b87a7df6f96a
Salik Hussain <salikhussain71@gmail.com>
2026-05-18 14:49:43 +0500

    Delete week02/week-02 directory

--- week02/week-02/day-03_if.py
+++
@@ -1,9 +0,0 @@
-x = int(input("what x? "))
-y = int(input("what's y? "))
-
-if x < y:
-    print("x is less than y")
-if x > y:
-    print("x is greater than y")
-if x == y:
-    print("x is equal to y")
